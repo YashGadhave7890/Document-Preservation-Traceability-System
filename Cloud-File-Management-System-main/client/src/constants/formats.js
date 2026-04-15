106c99d8e6858a7de41812b7ad23a0131f18435d
YASH GADHAVE <143506484+YashGadhave7890@users.noreply.github.com>
2026-04-15 20:33:34 +0530

    Delete Cloud-File-Management-System-main directory

--- Cloud-File-Management-System-main/client/src/constants/formats.js
+++
@@ -1,5 +0,0 @@
-export const videoFormats = ["mp4", "webm", "ogg", "avi", "mov", "mkv"];
-export const audioFormats = ["mp3", "wav", "ogg", "aac"];
-export const textFormats = ["txt", "xml", "json", "csv"];
-export const imageFormats = ["jpg", "jpeg", "png"];
-export const documentFormats = ["pdf", "doc", "docx", "ppt", "pptx", "xls"];
